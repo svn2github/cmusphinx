d69b0c72e68cdd9adbb0c771bac2ba6e0f830107
air <air@94700074-3cef-4d97-a70e-9c8c206c02f5>
2014-11-19 17:33:05 +0000

    additional error checking and warnings.
    git-svn-id: http://svn.code.sf.net/p/cmusphinx/code/trunk@12689 94700074-3cef-4d97-a70e-9c8c206c02f5

--- cmudict/scripts/test_cmudict.pl
+++ cmudict/scripts/test_cmudict.pl
@@ -60,19 +60,27 @@
 #
 
 use strict;
+use Getopt::Long;
+
 
 my ($word, $pron, $tok);
-my (%dict, %phone);
+my (%dict, %phone, %class);
+my $haslowercase = 0;
+my $problems=0;
 
-use Getopt::Std; use vars qw/ $opt_p $opt_n /;
-if ($#ARGV<0) { die("usage: test_dict -p <phonefile> <dictfile>\n"); }
-getopt('p:n:');
-my $phonefile = $opt_p;
-my $dictfile = $ARGV[0];
+my ($phonefile,$symbfile,$dictfile);
+GetOptions("phone=s" => \$phonefile, "dict=s"  => \$dictfile)
+    or die("usage: test_cmudict -p <phonefile> -d <dictfile>\n");
 
-# get the legal symbol set
+
+# get the legal symbol set (and class label)
 open(PH,$phonefile) || die("can't open $phonefile!\n");
-while (<PH>) { s/[\r\n]*//g; $phone{$_} = 0; }
+while (<PH>) {
+    s/[\r\n]*$//;
+    /(\w+)\t(\w+)/;
+    $phone{$1} = 0;
+    $class{$1} = $2;
+}
 close(PH);
 
 open(DICT,$dictfile) ||die("$dictfile not found!\n");
@@ -80,8 +88,8 @@ open(DICT,$dictfile) ||die("$dictfile not found!\n");
 # go through dict, do tests
 %dict = (); my $last = ""; my ($lead, $trail); my $word_cnt = 0;
 while (<DICT>) {
-    chomp;  #    s/^\s*(.+?)\s*$/$1/;
-    s/[\r\n]*$//g;  # deal with dos/unix diffs
+    # chomp;  #    s/^\s*(.+?)\s*$/$1/;
+    s/[\r\n]*$//;  # deal with dos/unix/ios annoyance
     if ( /^;;;/ ) { next; }  # ignore all comments in this pass
     my $line = $_;
     if ( $line  =~ /^\s*$/ ) { next; }  # empty lines allowed
@@ -89,46 +97,60 @@ while (<DICT>) {
     # general spacing issues
     ($lead = $_) =~ s/^\s*(.+)/$1/;
     ($trail = $_) =~ s/(.+?)\s*$/$1/;
-    if ($line ne $trail) { print "ERROR: trailing space in '$line'!\n"; }
-    if ($line ne $lead) { print "ERROR: leading space in '$line'!\n"; }
+    if ($line ne $trail) { print "ERROR: trailing space in '$line'!\n"; $problems++; }
+    if ($line ne $lead) { print "ERROR: leading space in '$line'!\n"; $problems++; }
 
     # examine the head term and the pronunciation
     ($word,$pron) = split (/\s+/,$line,2);
     $dict{$word}++;
+    if ( $word =~ /[a-z]/ ) { $haslowercase++; }
 
-    # check tabbing
+    # check tabbing (2 spaces)
     my @line = split (/  /,$line);
     if ( ($line[0] ne $word) or (scalar @line ne 2) ) { 
 	print "WARNING: tabbing error (",scalar @line, ") in: $line\n";
+	$problems++;
     }
 
     # check variant suffix
     # note that other than it being a digit, it only has to be unique
     if ( $word =~ /^[\(\)]/ ) {  # ignore 1st char if it's a paren
-	$tok = substr $word, 1;
+	$tok = substr $word, 1;  # this isn't right...
     } else { $tok = $word; }
     if ( ($tok =~ /\(/) or ($tok =~ /\)/) ) {
 	if ( not ($tok =~ /^.+?\(\d\)$/) ) {
-	    print "ERROR: malformed variant tag in '$word'\n";
+	    print "ERROR: malformed variant tag in '$word'\n"; $problems++;
 	}
     }
 
     # check for legal phonetic symbols
     my @sym = split(/\s/,$pron);
-    my $errs = "";
-    my ($ph,$stress,$tail);
+    my @errs = ();
+    my ($ph,$stress,$tail,$legalV);
     foreach my $s (@sym) {
-	$ph = $stress = 0;
-	($ph,$stress) = ($s =~ /([A-Z]+)([012]*)/);
-	if ( ( not defined $phone{$ph} )
-	    ) { $errs .= " $s"; } else { $phone{$s}++; }
+	$ph = $stress = $tail = '';
+	($ph,$stress,$tail) = ($s =~ /([A-Z]+)([012]*)(.*)$/);
+	$|=1;
+	# print join( '|', ($s =~ /([A-Z]+)([012]*)(.*)$/)),"\n";
+	$legalV =  defined $phone{$ph} && $class{$ph} eq 'vowel' && $stress ne '' && $tail eq '';
+	if ( not defined $phone{$ph} ) { push @errs, $s; }
+	elsif ( $legalV ) { $phone{$s}++; }  # doesn't do anything here, yet
+	else { $phone{$s}++; }  # could be a bare vowel...
+
+	if ( $tail ne '' ) { push @errs, $s; }
+	elsif ( (defined $class{$ph}) and ($class{$ph} eq 'vowel') and ($stress eq '') ){
+	    print "WARN: $word has a bare phone: $ph\n"; $problems++;
+	}
+	elsif ( (defined $class{$ph}) and ($class{$ph} ne 'vowel') and ($stress ne '') ){
+	    push @errs, $s; }
     }
-    if ($errs ne "") { print "ERROR: $word has illegal symbols: '$errs'\n"; }
+    if (scalar @errs ne 0) { print "ERROR: $word has unknown phone(s): '".
+			   join(' ',@errs)."'\n"; $problems++; }
 
 
     # word order
     if ( &strip_variant($last) gt &strip_variant($word) ) {
-	print "ERROR: collation sequence for $last, $word wrong\n";
+	print "*ERR:\tcollation sequence for $last, $word wrong\n"; $problems++;
 	print "\t",&strip_variant($last)," ?? ",&strip_variant($word),"\n";
     }
 
@@ -140,12 +162,18 @@ close(DICT);
 
 # check for duplicates entries
 foreach my $x (keys %dict) {
-    if ($dict{$x}>1) { print "ERROR: '$x' occurs ", $dict{$x}, " times!\n"; }
+    if ($dict{$x}>1) {
+	print "ERROR: '$x' occurs ", $dict{$x}, " times!\n";
+	$problems++;
+    }
 }
 
 print "\nprocessed $word_cnt words\n";
+if ( $haslowercase ne 0 ) { print "$haslowercase entries have lower case"; }
+if ($problems eq 0) { print "no problems encountered!\n"; }
 
 # print out the phone counts
+print "symbol occurence statistics:\n";
 $last = "";
 foreach (sort keys %phone) {
     if ( substr($_,0,2) eq substr($last,0,2) ) { print "\t| "; } else { print "\n  "; }
